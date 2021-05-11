b58cda2e5c47ff7f6e46a4f0b931bba91a4d4147
Kopal <kopaljain03@gmail.com>
2021-05-11 12:27:19 +0530

    added pages without mdification

---
+++ signup.php
@@ -0,0 +1,95 @@
+<?php
+
+$shoalert=false;
+$shoerror=false;
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  include 'partials/_dbconnect.php';
+$username=$_POST["username"];
+$password=$_POST["password"];
+$cpassword=$_POST["cpassword"];
+$exist=false;
+if(($password==$cpassword) && $exist==false){
+$shoalert=true;
+$sql="INSERT INTO `users` ( `username`, `password`, `dt`) VALUES ( '$username', '$password', current_timestamp())";
+$result=mysqli_query($conn,$sql);
+}
+else{
+$shoerror=true;
+$shoerror="The passwords do not match!";
+}
+}
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+   <style>
+   #signupform{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+   }
+   </style>
+
+    <title>SignUp page</title>
+  </head>
+  <body>
+
+    <?php require 'partials/_nav.php' ?> 
+
+    <?php
+  if($shoalert){
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong>Success!</strong> Your account has been created successfully.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>';
+  }
+  if($shoerror){
+    echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <strong>Error!</strong> '.$shoerror.'
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>';
+  }
+    ?>
+
+    <div class="container">
+    <br>
+    <h1 class="text-center">SignUp here</h1>
+
+    <form action='/login/signup.php' method='post' id='signupform'>
+  <div class="mb-3 col-md-6">
+    <label for="username" class="form-label">Username</label>
+    <input type="email" class="form-control " id="username" name="username" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3 col-md-6">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3 col-md-6">
+    <label for="cpassword" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="cpassword" name="cpassword">
+  </div>
+  <button type="submit" class="btn btn-primary">Signup</button>
+</form>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
